277eb38e62365f02c282af16dff1c468fc865e80
Ma, Jia-Jun <hoxide@gmail.com>
2024-01-25 12:25:51 +0800

    up

--- Coxeter/CoxterSystem.lean
+++ Coxeter/CoxterSystem.lean
@@ -1,9 +1,53 @@
 import Coxeter.OrderTwoGen
 
-local notation :max "ℓ(" g ")" => (length S g)
+namespace OrderTwoGen
+variable {G : Type*} [Group G] (S : Set G) [OrderTwoGen S]
+
+local notation:max "ℓ(" g ")" => (length S g)
 
 def ExchangeProp := ∀ (L:List S) (s:S) ,reduced_word L →
       ℓ(s * L) ≤ ℓ(L) → ∃ (i: Fin L.length), (s :G) * L= (L.removeNth i)
+
+
+def ExchangeProp' :=
+   ∀ {L : List S} {s : S },
+   reduced_word L → ℓ(( L * s)) ≤ ℓ(L) → ∃ (i: Fin L.length) ,(L:G) * s= (L.removeNth i)
+
+
+lemma exchange'_if_exchange : ExchangeProp S →  ExchangeProp' S:= by {
+   rw [ExchangeProp,ExchangeProp']
+   intro EP L s HL Hlen
+   let Lr := L.reverse
+   have HLr := reverse_is_reduced  HL
+   have Hlenr :ℓ(s * L.reverse)≤ ℓ(L.reverse) := by {
+      calc
+      _ =  ℓ((s * L.reverse)⁻¹) := length_eq_inv_length
+      _ =  ℓ(L * s) := by {
+         congr 1
+         nth_rewrite 1 [gprod_reverse,inv_eq_self' s]
+         group }
+      _ ≤  ℓ(L) := Hlen
+      _ =  ℓ(L.reverse) := by simp only [reverse_length_eq_length]
+   }
+   let ⟨i, Hp⟩  := EP Lr s HLr Hlenr
+   rw [<-gprod_cons] at Hp
+   let j : Fin L.length:= ⟨L.length -1 - i.1, by {
+      have : (0:ℕ)  < L.length := by {
+         calc
+         (0:ℕ)  ≤ i := by simp
+         _ < Lr.length := i.2
+         _ = L.length := by simp
+         }
+      calc
+      _ ≤  L.length - 1 := by sorry
+      _ < _ := by sorry
+   }⟩
+   use j
+
+   sorry
+}
+
+
 
 def DeletionProp := ∀ (L:List S), ℓ( L ) < L.length → ∃ (j: Fin L.length), ∃ (i:Fin j), (L:G)= ((L.removeNth j).removeNth i)
 
@@ -11,19 +55,19 @@ def DeletionProp := ∀ (L:List S), ℓ( L ) < L.length → ∃ (j: Fin L.length
 lemma exchange_imp_deletion : ExchangeProp S →  DeletionProp S := by {
    rw [ExchangeProp,DeletionProp]
    intro EP L HL
-   have  HL' := (length_lt_iff_non_reduced S  L).1 HL
-   let j := max_reduced_word_index'  L HL'
+   have  HL' : ¬ reduced_word L := (length_lt_iff_non_reduced).1 HL
+   let j := max_reduced_word_index' HL'
    use j
    let L1 := L.take j
    have Hj : L1.length = j := List.take_le_length L (le_of_lt j.2)
-   have red_L1 : reduced_word L1 := reduced_take_max_reduced_word  L HL'
+   have red_L1 : reduced_word L1 := reduced_take_max_reduced_word HL'
    let s := L.get j
    have nonred_L1p : ¬ reduced_word (L1++[s]) := by {
         rw [<-List.take_get_lt L j.1 j.2]
-        have := nonreduced_succ_take_max_reduced_word  L HL'
+        have := nonreduced_succ_take_max_reduced_word  HL'
         exact this
    }
-   have non_red_L1_s: ℓ((L1.gprod * s)) ≤  ℓ(L1.gprod) := reduced_nonreduced_length_le  L1 s red_L1 nonred_L1p
+   have non_red_L1_s: ℓ((L1 * s)) ≤  ℓ(L1) := reduced_nonreduced_length_le red_L1 nonred_L1p
    -- have ⟨i,HL1s⟩  := EP L1 s red_L1 non_red_L1_s
    -- use ⟨i,(by {rw [<-Hj]; exact i.2})⟩
    -- rw [List.remove_nth_eq_take_drop L j]
@@ -37,12 +81,11 @@ lemma exchange_imp_deletion : ExchangeProp S →  DeletionProp S := by {
 We now prove that ExchangeProp and DeletionProperty are equivalent
 -/
 
+end OrderTwoGen
 
-end orderTwoGen
-
-class CoxeterSystem {G : Type _} [Group G] (S : Set G) extends orderTwoGen S where
-  exchange : orderTwoGen.ExchangeProp S
-  deletion : orderTwoGen.DeletionProp S
+class CoxeterSystem {G : Type _} [Group G] (S : Set G) extends OrderTwoGen S where
+  exchange : OrderTwoGen.ExchangeProp S
+  deletion : OrderTwoGen.DeletionProp S
 
 namespace CoxeterSystem
 
--- Coxeter/OrderTwoGen.lean
+++ Coxeter/OrderTwoGen.lean
@@ -119,6 +119,9 @@ fun x hx => mul_eq_one_iff_eq_inv.1 (h.order_two x hx).1
 @[simp]
 lemma inv_eq_self' : ∀ (x : S),  x = (x:G)⁻¹ := fun x =>  inv_eq_self x.1 x.2
 
+@[simp]
+lemma inv_eq_self'' : ∀ (x : S),  (x:G)⁻¹ = x := fun x =>  Eq.symm (inv_eq_self x.1 x.2)
+
 -- The lemma was called non_one
 lemma gen_ne_one : ∀ x∈S, x ≠ 1 :=
 fun x hx => (h.order_two x hx).2
@@ -142,9 +145,13 @@ lemma reverse_prod_prod_eq_one {L: List S}  : (L.reverse :G) * L = 1:= by {
     _ = _ := inv_reverse_prod_prod_eq_one
 }
 
+@[simp]
 lemma gprod_reverse (L: List S) : L.reverse.gprod = (L.gprod)⁻¹ :=
  mul_eq_one_iff_eq_inv.1 reverse_prod_prod_eq_one
 
+
+@[simp]
+lemma gprod_reverse' (L: List S) : (L.reverse:G)⁻¹ = L := by simp
 
 lemma exists_prod (g : G) : ∃ (L : List S),  g = L := h.expression g
 
@@ -242,6 +249,8 @@ lemma length_eq_iff {L: List S} : reduced_word L ↔ L.length = ℓ(L.gprod) := 
    . intro H
      exact (length_le_iff).2 (le_of_eq H)
 
+
+
 lemma exists_reduced_word (S : Set G) [OrderTwoGen S] (g : G) : ∃ (L: List S) , reduced_word L ∧ g = L.gprod := by
    let ⟨L',h1,h2⟩ := Nat.find_spec (@length_aux G  _ S _ g)
    use L'
@@ -249,6 +258,29 @@ lemma exists_reduced_word (S : Set G) [OrderTwoGen S] (g : G) : ∃ (L: List S) 
    rw [length] at C1
    simp_rw [h2] at h1
    exact ⟨C1 h1,h2⟩
+
+
+lemma length_eq_inv_length: ℓ(g) = ℓ(g⁻¹) := by {
+  obtain ⟨L,HL1,HL2⟩ := exists_reduced_word S g
+  repeat rw [HL2]
+  calc
+  _ = L.length := by rw [<-length_eq_iff.1 HL1]
+  _ = L.reverse.length :=  by simp
+  _ = ℓ(L.reverse) := length_eq_iff.1 (reverse_is_reduced HL1)
+  _=_ := by simp [gprod_reverse]
+}
+
+@[simp]
+lemma inv_length_eq_length: ℓ(g⁻¹) = ℓ(g) := Eq.symm length_eq_inv_length
+
+lemma length_eq_reverse_length (L:List S): ℓ(L) = ℓ(L.reverse) := by {
+  calc
+  _ = ℓ((L:G)⁻¹) := length_eq_inv_length
+  _ = _ :=  by simp [gprod_reverse]
+}
+
+@[simp]
+lemma reverse_length_eq_length (L:List S): ℓ(L.reverse)=ℓ(L)  := Eq.symm (length_eq_reverse_length L)
 
 
 lemma length_cons {hd : S} {tail : List S} : ℓ(hd::tail) ≤ ℓ(tail) + 1 := by {
@@ -274,20 +306,20 @@ lemma max_reduced_word_index_aux (L: List S) (H : ¬ reduced_word L) : ∃ n, no
    rw [non_reduced_p,List.take_all_of_le (le_of_lt (Nat.lt_succ_self L.length))]
    exact H
 
-noncomputable def max_reduced_word_index (L : List S) (H : ¬ reduced_word L):= Nat.find (max_reduced_word_index_aux  L H)
+noncomputable def max_reduced_word_index {L : List S} (H : ¬ reduced_word L):= Nat.find (max_reduced_word_index_aux  L H)
 
-lemma nonreduced_succ_take_max_reduced_word (L : List S) (H : ¬ reduced_word L) : ¬ reduced_word (L.take ((max_reduced_word_index  L H)+1)) := by
-   let j:= max_reduced_word_index  L H
-   have Hj : j = max_reduced_word_index  L H := rfl
+lemma nonreduced_succ_take_max_reduced_word {L : List S} (H : ¬ reduced_word L) : ¬ reduced_word (L.take ((max_reduced_word_index  H)+1)) := by
+   let j:= max_reduced_word_index  H
+   have Hj : j = max_reduced_word_index H := rfl
    rw [<-Hj]
    rw [max_reduced_word_index]  at Hj
    have HH:= (Nat.find_eq_iff _).1 Hj
    rw [<-Hj,non_reduced_p] at HH
    exact HH.1
 
-lemma reduced_take_max_reduced_word (L : List S) (H : ¬ reduced_word L) : reduced_word (L.take (max_reduced_word_index L H)) := by
-   let j:= max_reduced_word_index L H
-   have Hj : j = max_reduced_word_index  L H := rfl
+lemma reduced_take_max_reduced_word {L : List S} (H : ¬ reduced_word L) : reduced_word (L.take (max_reduced_word_index H)) := by
+   let j:= max_reduced_word_index H
+   have Hj : j = max_reduced_word_index H := rfl
    match j with
    | 0 =>
       rw [<-Hj,List.take_zero]
@@ -298,7 +330,7 @@ lemma reduced_take_max_reduced_word (L : List S) (H : ¬ reduced_word L) : reduc
       rw [non_reduced_p,not_not] at this
       exact this
 
-lemma max_reduced_word_index_lt {L : List S} (H : ¬ reduced_word L) : max_reduced_word_index L H < L.length := by
+lemma max_reduced_word_index_lt {L : List S} (H : ¬ reduced_word L) : max_reduced_word_index H < L.length := by
    have Hlen := pos_length_of_non_reduced_word H
    rw [max_reduced_word_index, Nat.find_lt_iff _ L.length]
    use L.length -1
@@ -310,7 +342,7 @@ lemma max_reduced_word_index_lt {L : List S} (H : ¬ reduced_word L) : max_reduc
      rw [this,List.take_length]
      exact H
 
-noncomputable def max_reduced_word_index' (L : List S) (H : ¬ reduced_word L) : Fin L.length:= ⟨max_reduced_word_index  L H, max_reduced_word_index_lt  H⟩
+noncomputable def max_reduced_word_index' {L : List S} (H : ¬ reduced_word L) : Fin L.length:= ⟨max_reduced_word_index H, max_reduced_word_index_lt  H⟩
 
 lemma length_lt_iff_non_reduced {L : List S} : ℓ(L) < L.length ↔ ¬ reduced_word L := by {
    rw [iff_not_comm,not_lt]
@@ -336,5 +368,16 @@ lemma reduced_imp_tail_reduced : reduced_word (L : List S) → reduced_word L.ta
     linarith
   }
 
+
+
+lemma reduced_nonreduced_length_le  {L : List S} {s : S} (H1: reduced_word L) (H2: ¬ reduced_word (L ++ [s])) :ℓ(L.gprod * s) ≤ ℓ(L.gprod) := by {
+    rw [<-(length_eq_iff).1 H1]
+    contrapose H2
+    have Hs :[s].gprod = s := gprod_singleton
+    have Hlen : (L++[s]).length = Nat.succ L.length := by rw [List.length_append, List.length_singleton]
+    rw [not_le,←gprod_singleton,←gprod_append,<-Nat.succ_le_iff,<-Hlen] at H2
+    rw [not_not,length_le_iff]
+    exact H2
+}
 
 end OrderTwoGen
