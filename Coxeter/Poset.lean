0d64eb0c3bb90f483efa2078b695fa472fb274d6
huanchen_95b6 <huanchen@nus.edu.sg>
2024-03-04 20:46:15 +0800

    Posets

--- Coxeter/Poset.lean
+++ Coxeter/Poset.lean
@@ -2,7 +2,30 @@ import Coxeter.CoxeterSystem
 
 import Mathlib.Data.Set.Card
 
+namespace Shelling
 
-variable {P : Type*} [PartialOrder P]
+variable {P : Type*} [PartialOrder P] [Finite P]
 
-def ledot (a b : P) := a < b ∧ (∀ c : P, a ≤ c ≤ b, (a = c ∨ b = c))
+def ledot (a b : P) := a < b ∧ (∀ {c : P}, (a ≤ c ∧ c ≤ b) → (a = c ∨ b = c))
+
+def edges {P : Type*} [PartialOrder P] : Set (P × P) := { (a, b) | ledot a b }
+
+def EL {P A : Type*} [PartialOrder P] [PartialOrder A] := edges P → A
+
+def maximal_chain {P : Type*} [PartialOrder P] : List P := sorry
+
+def graded {P : Type*} [PartialOrder P] : ∀ (L₁ L₂ : List P), ((maximal_chain L₁) ∧ (maximal_chain L₂)) → (L₁.length = L₂.length)
+
+def shellable {P : Type*} [PartialOrder P] : sorry
+
+def CL_labelling {P : Type*} [PartialOrder P] : sorry
+
+def EL_labelling {P : Type*} [PartialOrder P] : sorry
+
+theorem CL_shellable {P : Type*} [PartialOrder P] (h: CL_labelling P) : shellable P := by sorry
+
+theorem EL_CL {P : Type*} [PartialOrder P] (h: EL_labelling P) : CL_labelling P := by sorry
+
+theorem EL_shellable {P : Type*} [PartialOrder P] (h: EL_labelling P) : shellable P := by
+  apply EL_CL at h
+  apply CL_shellable h
