d0ee92b285ac21e1b39a55798b553a6fedb2481d
zsj <2951037717@qq.com>
2024-04-10 16:20:52 +0800

    update

--- Coxeter/Rpoly.lean
+++ Coxeter/Rpoly.lean
@@ -17,7 +17,7 @@ local notation : max "q⁻¹" => @LaurentPolynomial.T ℤ _ (-1)
 #check SimpleRefl_is_Refl
 
 -- trans to ...
-lemma length_induction {p : G → Prop} {w: G} {s:rightDescent w} (hw: Nonempty (rightDescent w)) (h1 : p 1) (hws : p (w*s) → p w) : ∀ u:G, p u := sorry
+lemma length_induction {p : G → Prop} {w: G} {s:rightDescent w} (hw: w ≠ 1) (h1 : p 1) (hws : p (w*s) → p w) : ∀ u:G, p u := sorry
 
 -- trans to CoxeterSystem
 lemma mul_SimpleRefl_twice (w:G) (s: hG.S) : w = w*s*s := by
@@ -30,6 +30,10 @@ lemma mem_rD_mul_of_not_mem_rD {w:G} {s: hG.S} (h: s.1 ∉ rightDescent w) : s.1
   sorry
 
 --trans to BruhatOrder
+lemma eq_one_of_le_one {w:G} : w ≤ 1 → w = 1 := by
+  intro h
+  sorry
+
 lemma mul_lt_of_mem_rD {v :G} {s: hG.S} (h: s.1 ∈ rightDescent v) : v*s < v := by
   have hlt : lt_adj (v*s) v := by
     rw [lt_adj]
@@ -41,12 +45,22 @@ lemma mul_gt_of_not_mem_rD {v :G} {s: hG.S} (h: s.1 ∉ rightDescent v) : v < v*
   nth_rw 1 [mul_SimpleRefl_twice v s]
   exact mul_lt_of_mem_rD (mem_rD_mul_of_not_mem_rD h)
 
-lemma le_cancel_of_mem_rD {u v :G} {s: hG.S} (h: s.1 ∈ rightDescent v) : u*s ≤ v*s → u ≤ v := by
-  sorry
+lemma le_cancel_of_not_mem_rD {u v :G} {s: hG.S} (h1: s.1 ∉ rightDescent u) (h2: s.1 ∈ rightDescent v) :
+  u*s ≤ v*s → u ≤ v := by
+    have h := le_of_lt (mul_gt_of_not_mem_rD h1)
+    have h' := le_of_lt (mul_lt_of_mem_rD h2 )
+    intro h''
+    exact le_trans (le_trans h h'') h'
+
+--use subword property
+lemma le_cancel_of_mem_rD {u v :G} {s: hG.S} (h1: s.1 ∈ rightDescent u) (h2: s.1 ∈ rightDescent v) :
+  u*s ≤ v*s → u ≤ v := by
+    sorry
 
 -- trans to Hecke
 lemma muls_apply_antidiagonal_of_memrD (h : Hecke G) (s : hG.S) (w : G) (h1 : ℓ(w*s) < ℓ(w)) :
-    (h * TT s.1) w = (q-1) * h w + h (w*s) := sorry
+  (h * TT s.1) w = (q-1) * h w + h (w*s) := by
+    sorry
 
 lemma muls_apply_antidiagonal_of_not_memrD (h : Hecke G) (s : hG.S) (w : G) (h1 : ℓ(w) < ℓ(w*s)) :
     (h * TT s.1) w = q * h (w*s) := sorry
@@ -86,10 +100,12 @@ lemma Rpoly_aux {u v :G} (h1:s ∈ rightDescent v) (h2:s ∈ rightDescent u):
       have hl : ℓ((v * s)⁻¹) < ℓ(s * (v * s)⁻¹) := sorry
       nth_rw 1 [mul_SimpleRefl_twice u ⟨s,sss⟩ ,mul_SimpleRefl_twice v ⟨s,sss⟩]
       rw [mul_inv_rev,←inv_eq_self' ⟨s,sss⟩,TTInv_muls_of_length_gt' ⟨s,sss⟩ hl]
-      --rw [TTInv_s_eq,mul_sub (TTInv (v * s)⁻¹) (q⁻¹ • TT s) (1 - q⁻¹) • 1]
+      rw [TTInv_s_eq]
+      -- simp only [Subtype.coe_mk]
+      -- rw [mul_sub (TTInv (v * s)⁻¹) (q⁻¹ • TT s) (1 - q⁻¹) • 1]
       sorry
 
-lemma Rpoly_memrD : ∀(u v:G),s ∈ rightDescent v → s ∈ rightDescent u → R G u v = R G (u*s) (v*s) := by
+lemma Rpoly_mem_rD : ∀(u v:G),s ∈ rightDescent v → s ∈ rightDescent u → R G u v = R G (u*s) (v*s) := by
   intro u v h1 h2
   by_cases hn : Nonempty (rightDescent v)
   · by_cases hn' : Nonempty (rightDescent u)
@@ -135,13 +151,6 @@ lemma Rpoly_not_mem_rD : ∀(u v:G),s ∈ rightDescent v → s ∉ rightDescent 
     · have : Nonempty (rightDescent v) := Nonempty.intro ⟨s,hsv⟩
       contradiction
 
--- lemma Rpoly_not_le_aux : ∀ l, ∀(u v:G), l = ℓ(v) → R G u v ≠ 0 → u ≤ v := by
---   intro l
---   intro u v hl
---   induction' l with n hn
---   · sorry
---   · sorry
-
 lemma Rpoly_not_le : ∀(u v:G), ¬ (u ≤ v) → R G u v = 0 := by
   intro u v
   by_cases hv : v = 1
@@ -160,61 +169,27 @@ lemma Rpoly_not_le : ∀(u v:G), ¬ (u ≤ v) → R G u v = 0 := by
     let p := fun v => (∀ u, ¬u ≤ v → R G u v = 0)
     have hws : (∀ u, ¬u ≤ (v*s) → R G u (v*s) = 0) → (∀ u, ¬u ≤ v → R G u v = 0) := by
       intro h u huv
+      have sss : s.1 ∈ hG.S := Set.mem_of_mem_inter_right s.2
       by_cases hsv : s.1 ∈ (rightDescent u)
-      · rw [Rpoly_memrD u v s.2 hsv]
+      · rw [Rpoly_mem_rD u v s.2 hsv]
         contrapose huv
         have h2 := Function.mt (h (u*s)) huv
         push_neg at *
-        sorry
+        exact le_cancel_of_mem_rD (s:= ⟨s.1,sss⟩) hsv s.2 h2
       · rw [Rpoly_not_mem_rD u v s.2 hsv]
-        have himp1 : u*s ≤ v*s → u ≤ v := sorry
-        have himp2 : u ≤ v*s → u ≤ v := sorry
+        have himp1 : u*s ≤ v*s → u ≤ v := by
+          intro hh
+          exact le_cancel_of_not_mem_rD (s:= ⟨s.1,sss⟩) hsv s.2 hh
+        have himp2 : u ≤ v*s → u ≤ v := by
+          intro hh
+          have := le_of_lt (mul_lt_of_mem_rD (s:= ⟨s.1,sss⟩) (v:=v) s.2)
+          rw [Subtype.coe_mk] at this
+          exact le_trans hh this
         have hi1 := h (u*s) (Function.mt himp1 huv)
         have hi2 := h u (Function.mt himp2 huv)
         rw [hi1,hi2]
         simp
-    exact length_induction (p:=p) hrDv h1 hws v u
-
--- lemma Rpoly_memrD : ∀(u v:G),s ∈ rightDescent v → s ∈ rightDescent u → R G u v = R G (u*s) (v*s) := by
---   intro u v h1 h2
---   by_cases hn : Nonempty (rightDescent v)
---   · by_cases hn' : Nonempty (rightDescent u)
---     · rw [R,R]
---       have hlvs : ℓ(v*s) + 1 = ℓ(v) := sorry
---       by_cases huv : u ≤ v
---       · have usvs : u * s ≤ v * s := sorry
---         have t1:= eq_true huv
---         have t2:= eq_true usvs
---         simp [t1,t2]
---         rw [←mul_inv_rev,←Rpoly_aux h1 h2]
---         have hlusv : ℓ(v) + ℓ(u) = ℓ(v*s) + ℓ(u*s) + 2:=sorry
---         rw [hlusv,pow_add,neg_one_pow_two,mul_one,←hlvs,pow_add q,pow_one]
---         ring
---       · have usvs : ¬(u * s ≤ v * s) := sorry
---         have f1 := eq_false huv
---         have f2 := eq_false usvs
---         simp [f1,f2]
---     · have : Nonempty (rightDescent u) := Nonempty.intro ⟨s,h2⟩
---       contradiction
---   · have : Nonempty (rightDescent v) := Nonempty.intro ⟨s,h1⟩
---     contradiction
-
--- lemma Rpoly_not_mem_rD : ∀(u v:G),s ∈ rightDescent v → s ∉ rightDescent u →
---   R G u v = q*R G (u*s) (v*s) + (q-1) * R G u (v*s) := by
---     intro u v hsv hsu
---     by_cases hn : Nonempty (rightDescent v)
---     · repeat
---         rw [R]
---       by_cases huv : u ≤ v
---       · have h1 : u < v := sorry
---         have h2 : u ≤ v * s := sorry
---         by_cases huv' : (u*s) ≤ (v*s)
---         · have t1 := eq_true huv
---           have t2 := eq_true h2
---           have t3 := eq_true huv'
---           simp [t1,t2,t3]
---     · have : Nonempty (rightDescent v) := Nonempty.intro ⟨s,hsv⟩
---       contradiction
+    exact length_induction (p:=p) hv h1 hws v u
 
 lemma Rpoly_eq' : ∀ l, ∀ w : G, l = ℓ(w) → TTInv w⁻¹ w = q⁻¹^(ℓ(w)) := by
   intro l
@@ -248,5 +223,34 @@ lemma Rpoly_eq : ∀ (u : G), R G u u = 1 := by
 instance : Rpoly (R G) where
   not_le := Rpoly_not_le
   eq := Rpoly_eq
-  mem_rD := Rpoly_memrD
+  mem_rD := Rpoly_mem_rD
   not_mem_rD := Rpoly_not_mem_rD
+
+
+lemma Unique_Rpoly {R' : G → G → LaurentPolynomial ℤ}: Rpoly R' → R' = R G := by
+  intro h
+  funext u v
+  have h1 : ∀ u, R' u 1 = R G u 1 := by
+    intro u
+    by_cases h' : u = 1
+    · rw [h',Rpoly.eq (R:=R'),Rpoly_eq]
+    · have h'' : ¬ u ≤ 1 := by
+        contrapose h'
+        push_neg at *
+        exact eq_one_of_le_one h'
+      rw [Rpoly.not_le (R:=R'),Rpoly_not_le _ _ h'']
+      assumption
+  by_cases hv1 : v = 1
+  · rw [hv1]
+    exact h1 u
+  · let s := choice (rightDescent_NE_of_ne_one hv1)
+    let p := fun v => ∀ u, R' u v = R G u v
+    have hws : (∀ u, R' u (v*s) = R G u (v*s))→ ∀ u, R' u v = R G u v := by
+      intro h'
+      intro u
+      by_cases hus : s.1 ∈ rightDescent u
+      · rw [Rpoly.mem_rD (R:=R') _ _ s.2 hus,Rpoly_mem_rD _ _ s.2 hus]
+        exact h' (u*s)
+      · rw [Rpoly.not_mem_rD (R:=R') _ _ s.2 hus,Rpoly_not_mem_rD _ _ s.2 hus]
+        rw [h' (u*s),h' u]
+    exact length_induction (p:=p) hv1 h1 hws v u
