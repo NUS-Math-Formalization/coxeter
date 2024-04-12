0c56a26d4cfe41f54ae75952b115d8cdcb798c9b
huanchen_95b6 <huanchen@nus.edu.sg>
2024-04-12 10:01:31 +0800

    New folder

---
+++ Coxeter/CoxeterMatrix/1.lean
@@ -0,0 +1,16 @@
+import Mathlib.Data.Matrix.Basic
+import Mathlib.GroupTheory.OrderOfElement
+import Mathlib.GroupTheory.PresentedGroup
+import Mathlib.GroupTheory.Perm.Basic
+import Mathlib.Data.Matrix.Notation
+import Mathlib.GroupTheory.PresentedGroup
+import Mathlib.LinearAlgebra.Matrix.Symmetric
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
+import Mathlib.Data.Complex.Exponential
+import Mathlib.RingTheory.RootsOfUnity.Basic
+
+import Coxeter.CoxeterSystem
+import Coxeter.OrderTwoGen
+import Coxeter.AlternatingWord
+
+open BigOperators
