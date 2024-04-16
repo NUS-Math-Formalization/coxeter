2add3f135773add23a86ae88f2ad9e9b87eee7eb
timechess <tyc221@ruc.edu.cn>
2024-04-17 00:08:51 +0800

    mem_adjPairs_iff

--- Coxeter/ForMathlib/AdjacentPair.lean
+++ Coxeter/ForMathlib/AdjacentPair.lean
@@ -7,6 +7,7 @@ variable {α : Type*}
 /- Definition: The adjacent pairs of a list [a_1,a_2, ⋯, a_n] is defined to be
   [(a_1, a_2), (a_2, a_3), ⋯, (a_{n-1}, a_n)].
   If the list L has length less than 2, the new list will be an empty list by convention. -/
+@[simp]
 def adjPairs : List α  → List (α × α)
   | [] => []
   | _ :: []  => []
@@ -28,8 +29,38 @@ lemma adjPairs_tail {h a b : α} {tail : List α} : (a,b) ∈ tail.adjPairs → 
     intro h1
     right; exact h1
 
+
 lemma mem_adjPairs_iff {a b : α} {L : List α} : (a,b) ∈ L.adjPairs ↔ ∃ l₁ l₂ : List α, L = l₁ ++ a :: b :: l₂ := by
-  sorry
+  constructor
+  · intro h
+    induction L with
+    | nil => simp at h
+    | cons e tail he =>
+        by_cases h' : (a, b) ∈ tail.adjPairs
+        · have := he h'
+          rcases this with ⟨l₁, l₂, hl⟩
+          use e :: l₁, l₂
+          simp [hl]
+        · match tail with
+          | [] => simp at h
+          | f :: tail' =>
+              simp at h
+              rcases h with h'' | h''
+              · simp [h'']
+                use [], tail'
+                simp
+              · contradiction
+  · intro h
+    rcases h with ⟨l₁, l₂, h'⟩
+    rw [h']
+    revert L
+    induction l₁ with
+    | nil => simp
+    | cons e tail he =>
+        intro L hL
+        have aux : L.tail = tail ++ a :: b :: l₂ := by simp [hL]
+        have := he aux
+        apply adjPairs_tail this
 
 
 /- Definition (programming):
