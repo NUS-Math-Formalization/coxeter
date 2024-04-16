ffdbc4a6eed1cee1aa95bc156eefab2b9c14b0e9
jjdishere <emailboxofjjd@163.com>
2024-04-17 01:26:30 +0800

    Update GraphSearchAux.lean

--- Coxeter/GraphSearchAux.lean
+++ Coxeter/GraphSearchAux.lean
@@ -69,3 +69,143 @@ def a1_dup_labeling (a : Fin 4) : List (Fin 4 × String) :=
 -- #eval depthFirstLabeling a1_labeling 2
 -- #eval depthFirstLabelingNoDup a1_dup_labeling 3 [3]
 -- #eval depthFirst a1 3
+
+namespace Attempt1
+
+variable [BEq α] [LawfulBEq α]
+-- #synth Decidable (1 ∈ [1,3])
+
+def next (g : α → List α) (last : α) (current : Option α) : Option α :=
+  match current  with
+  | .none => (g last).head?
+  | .some a =>
+    match (g last).indexOf? a  with
+    | .none => .none -- impossible case if our input is legal
+    | .some n => (g last).get? (n+1)
+
+partial def nodup_next (g : α → List α) (path : List α) (current : Option α) : Option α :=
+  match current, path with
+  | .some _, [] => .none -- impossible
+  | .some a, z :: _ =>
+    match next g z a with
+    | .none => .none
+    | .some b =>
+      if b ∈ path then
+        nodup_next g path b
+      else
+        .some b
+  | .none, [] => .none -- impossible
+  | .none, z :: _ =>
+    match (g z).head? with
+    | .none => .none
+    | .some b =>
+      if b ∈ path then
+        nodup_next g path b
+      else
+        .some b
+
+partial def nodup_g (g : α → List α) (path : List α) (current : Option α): List α :=
+  let new := (nodup_next g path current)
+  match new with
+  | .some b  =>
+    b :: nodup_g g path new
+  | .none =>
+    []
+
+def nodup_g' (g : α → List α) (path : List α) (current : α) : List α := nodup_g g (current :: path) .none
+
+def g₅ (x : Fin 5) : List (Fin 5) :=
+  match x with
+  | 0 => [1, 2]
+  | 1 => [0, 2 , 3]
+  | 2 => [0, 1 , 3 , 4]
+  | 3 => [1, 2]
+  | 4 => [2]
+
+#eval nodup_g' g₅ ([0]: List (Fin 5)) (2)
+
+/- A Depth-first graph search based on List. For the time being, I fail to adapt the nodup version. -/
+partial def depthFirstList' (g : List α → α → List α) (path : List α) (start : α) : List (List α) :=
+  match g path start with
+  | [] => [start :: path]
+  | _ =>
+    let new_path := start :: path
+    new_path :: (List.join $ List.map (depthFirstList' g new_path) (g new_path start))
+
+#eval depthFirstList' (nodup_g' g₅)  ([] : List (Fin 5)) 0
+
+end Attempt1
+
+
+
+
+
+
+
+
+namespace Attempt2
+
+variable [BEq α] [LawfulBEq α]
+-- #synth Decidable (1 ∈ [1,3])
+
+def next (g : α → List α) (last : α) (current : Option α) : Option α :=
+  match current  with
+  | .none => (g last).head?
+  | .some a =>
+    match (g last).indexOf? a  with
+    | .none => .none -- impossible case if our input is legal
+    | .some n => (g last).get? (n+1)
+
+partial def nodup_parallel_next (g : α → List α) (path : List α) (visited : List α) (current : Option α) : Option α := -- path and visited will not be changed in this function
+  match current, path with
+  | .some _, [] => .none -- impossible
+  | .some a, z :: _ =>
+    match next g z a with
+    | .none => .none
+    | .some b =>
+      if b ∈ visited then
+        nodup_parallel_next g path visited b
+      else
+        .some b
+  | .none, [] => .none -- impossible
+  | .none, z :: _ =>
+    match (g z).head? with
+    | .none => .none
+    | .some b =>
+      if b ∈ visited then
+        nodup_parallel_next g path visited b
+      else
+        .some b
+
+partial def nodup_next (g : α → List α) (path : List α) (visited : List α) (current : α) : Option α := -- visited and path does not include current, visited should contain path
+  match g current with
+  | [] => nodup_parallel_next g path visited current
+  | a :: _ =>
+    if a ∈ visited then
+      nodup_parallel_next g (current :: path) (current :: visited) a
+    else
+      a
+
+def g₅ (x : Fin 5) : List (Fin 5) :=
+  match x with
+  | 0 => [1, 2]
+  | 1 => [0, 2 , 3]
+  | 2 => [0, 1 , 3 , 4]
+  | 3 => [1, 2]
+  | 4 => [2]
+
+#eval nodup_next g₅ (path := [0,1]) (visited := [0,1, 3, 4]) (2)
+
+/- A Depth-first graph search based on List. For the time being, I fail to adapt the nodup version. -/
+partial def depthFirstList' (g : List α → List α → α → List α) (path : List α) (visited : List α) (current : α) : (List α) :=
+  match path, nodup_next (g visited path) path visited current with
+  | [], .none => current :: depthFirstList'
+  | [], .some a => sorry
+  | z :: past_path, .none => sorry
+  | z :: past_path, .some a => sorry
+
+
+#eval depthFirstList' (nodup_g' g₅)  ([] : List (Fin 5)) 0
+
+
+end Attempt2
