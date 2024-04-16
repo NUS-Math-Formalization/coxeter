945dfa4a4ec9165b3f74f99b02cb8a0b5be80a3d
jjdishere <emailboxofjjd@163.com>
2024-04-16 13:59:02 +0800

    Update Defs.lean

--- Coxeter/ReflectionAlgebra/Defs.lean
+++ Coxeter/ReflectionAlgebra/Defs.lean
@@ -7,4 +7,4 @@ open Classical
 local notation "ℛ" => LaurentPolynomial ℤ
 local notation "√q" => (LaurentPolynomial.T 1 : ℛ) --`q½`
 
-def genericHeckeAlg (G : Type*) [cox : CoxeterGroup G] :=
+def genericHeckeAlg (G : Type*) [cox : CoxeterGroup G] := G
