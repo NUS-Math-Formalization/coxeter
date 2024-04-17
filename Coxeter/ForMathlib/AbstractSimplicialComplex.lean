594524d94d2ad6356bbb0c6108bb26c407670b9f
lpya942 <mec2009470@xmu.edu.my>
2024-04-17 14:30:31 +0800

    i love coxeter groups(bushi )

--- Coxeter/ForMathlib/AbstractSimplicialComplex.lean
+++ Coxeter/ForMathlib/AbstractSimplicialComplex.lean
@@ -12,6 +12,8 @@ structure AbstractSimplicialComplex (V : Type*)  where
   empty_mem : ∅ ∈ faces
   lower' : IsLowerSet faces -- The set of faces is a lower set under the inclusion relation.
 
+theorem AbstractSimplicialComplex.nonempty {V : Type*} {F : AbstractSimplicialComplex V} : F.faces.Nonempty := Set.nonempty_of_mem F.empty_mem
+
 theorem isLowerSet_singleton_empty (α : Type*):
 IsLowerSet {(∅ : Set α)} := by
   intro _ _ blea ain
@@ -162,22 +164,110 @@ Definition: For any ASC F, we denote by Facets F the set of facets of F.
 -/
 def Facets (F : AbstractSimplicialComplex V) : Set (Finset V) := { s | F.IsFacet s}
 
-/-- Definition: A pure abstract simplicial complex is an abstract simplicial complex where all facets have the same cardinality. -/
-def IsPure (F : AbstractSimplicialComplex F) :=
+/-
+theorem Facets_nonempty {F : AbstractSimplicialComplex V} : Nonempty F.Facets := by
+  by_cases h : F.faces = {∅}
+  · have : ∀ s ∈ F, ∅ = s := by
+      intro s hs
+      rw [←mem_faces,h] at hs
+      rw [eq_comm]; exact Set.eq_of_mem_singleton hs
+    have hemp: IsFacet F ∅ := by
+      simp [IsFacet]
+      exact ⟨F.empty_mem,this⟩
+    have empsub : ∅ ∈ F.Facets := by
+     simp [Facets]
+     assumption
+    exact Nonempty.intro ⟨∅,empsub⟩
+    --确实有一个极大元s存在的证明
+  · have aux : ∃ s, s ∈ F ∧  ∀ t ∈ F, s ⊆ t → s = t := by sorry
+    --感觉是不太对 根据structure定义 faces : Set (Finset V) -- the set of faces
+    --如果吃无限个Finset V，那face就变成了infinite set，怎么取到这个maximal element
+
+    /-V : Type u_1
+      F : AbstractSimplicialComplex V
+      h : ¬F.faces = {∅}
+      --这个其实看着比较奇怪，理论上这句话后应该有两种情况，
+         F.faces这个集族真的是空集，貌似根据structure的定义可以排除
+         F.faces是含有∅及其他元素的集族
+      aux : ∃ s ∈ F, ∀ t ∈ F, s ⊆ t → s = t
+⊢ Nonempty ↑(Facets F)-/
+
+    rcases aux with ⟨s, hs, h⟩
+    have h1: IsFacet F s := ⟨hs,h⟩
+    exact Nonempty.intro ⟨s,h1⟩
+
+    /- 比较弱智的证明，在这里无脑simp之后的根据等式操作只有在lean中才能出现，
+       自然语言中显然不会有人想到这么细节的数理结构
+    simp[IsFacet] at h1
+    simp[Facets,IsFacet]
+    exact⟨ s, h1⟩
+    --尖括号是一个structure，可以看到
+
+    s : Finset V
+    hs : s ∈ F
+    h : ∀ t ∈ F, s ⊆ t → s = t
+    h1 : s ∈ F ∧ ∀ t ∈ F, s ⊆ t → s = t
+    ⊢ ∃ s ∈ F, ∀ t ∈ F, s ⊆ t → s = t
+
+    对于⟨ s, h1⟩相当于直接带入
+         [s : Finset V ]  为goal中的 [∃ s ∈ F] ,
+         [h1 : s ∈ F ∧ ∀ t ∈ F, s ⊆ t → s = t中的and符右边部分] 为goal中的  [∀ t ∈ F, s ⊆ t → s = t]
+    因而证明完成
+       -/
+
+-/
+/-- Definition: A pure abstract simplicial complex is an abstract simplicial complex
+    where all facets have the same cardinality. -/
+def IsPure (F : AbstractSimplicialComplex V) :=
   ∀ s∈ Facets F, ∀ t ∈ Facets F, s.card = t.card
 
-class Pure (F : AbstractSimplicialComplex F) where
+class Pure (F : AbstractSimplicialComplex V) where
   pure : ∀ s ∈ F.Facets, ∀ t ∈  F.Facets, s.card = t.card
 
 /--Definition: We will call an ASC pure of rank `d` if all its facets has `d` elements-/
-def IsPure' (F : AbstractSimplicialComplex F) (d : ℕ):=
-  ∀ s ∈ F.faces, s.card = d
+def IsPure' (F : AbstractSimplicialComplex V) (d : ℕ):=
+  --∀ s ∈ F.faces, s.card = d
+  --这个我真心感觉错了，怎么可能Pure‘里面是Facets这里IsPure'变成了faces
+  ∀ s ∈ F.Facets, s.card = d
 
-class Pure' (F : AbstractSimplicialComplex F) (d :ℕ) where
+class Pure' (F : AbstractSimplicialComplex V) (d :ℕ) where
   pure : ∀ s ∈ F.Facets, s.card = d
 
-lemma isPure_iff_isPure' {F : AbstractSimplicialComplex F} : F.IsPure ↔ ∃ d, F.IsPure' d := by
-  sorry
+lemma isPure_iff_isPure' {F : AbstractSimplicialComplex V} : F.IsPure ↔ ∃ d, F.IsPure' d := by
+  by_cases hemp : Nonempty F.Facets
+  · constructor
+    · let s := Classical.choice (hemp)
+      intro hIp
+      use s.1.card
+      intro t ht
+      exact hIp t ht s s.2
+    · intro hIp'
+      obtain ⟨d, hIp'⟩ := hIp'
+      -- rcases hIp' with ⟨d, hIp'⟩ 在这里用rcases和obtain效果是完全一样的
+      intro s hs t ht
+      rw [hIp' s hs, hIp' t ht]
+  · constructor
+    · intro
+      use 0
+      simp only [IsPure', Finset.card_eq_zero]
+      contrapose! hemp
+      rcases hemp with ⟨d, ⟨a, b⟩⟩
+      use d
+    · intro h
+      rcases h with ⟨d, a⟩
+      simp only [nonempty_subtype, not_exists] at hemp
+      intro s hs t _
+      exfalso
+      exact hemp s hs
+
+
+
+
+
+
+
+
+
 
 
 lemma pure_def {F : AbstractSimplicialComplex V} [Pure F] : ∀ s ∈ F.Facets, ∀  t ∈ F.Facets,  s.card = t.card := Pure.pure
@@ -189,6 +279,7 @@ If the size of simplices in F is unbounded, it has rank 0 by definition.
 
 Remark: We should general be careful with the unbounded case.
 -/
+
 noncomputable def rank (F : AbstractSimplicialComplex V) : ℕ := iSup fun s : F.faces => s.1.card
 
 /-- Definition: For a collection s of subsets of V, we denote by closure s the smallest ASC over V containing all elements in s
@@ -228,11 +319,13 @@ lemma closure_mono {s t: Set (Finset V)} : s ⊆ t → closure s ≤ closure t :
 
 lemma closure_le {F : AbstractSimplicialComplex V} (h: s ⊆ F.faces) : closure s ≤ F := by sorry
 
+
 /--
 Definition: G is a cone over F with cone point x if
 x ∈ G.vertices - F.vertices
 s ∈ F ⇔ s ∪ {x} ∈ G.
 -/
+
 def Cone (F G: AbstractSimplicialComplex V) (x : V) :=
   x ∈ G.vertices \ F.vertices ∧
   ∀ s, s ∈ F.faces ↔ s ∪ {x} ∈ G.faces
