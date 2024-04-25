3a2e6db372e5b621cd3663e2930efff1fbd2fc16
Mitchell Lee <trivial171@gmail.com>
2024-04-25 17:01:49 +0800

    add material about roots

--- Coxeter/CoxeterMatrix/Characterization'.lean
+++ Coxeter/CoxeterMatrix/Characterization'.lean
@@ -38,3 +38,54 @@ Given an `OrderTwoGen` group, we say the system satisfy the deletion property if
 given an expression `w = s₁ s₂ ⋯ sₙ ∈ G`, there exists `1 ≤ i, j < n` such that
 `w = s₁ ⋯ sᵢ₋₁ sᵢ₊₁ ⋯ sⱼ₋₁ sⱼ₋₁ ⋯ sₙ` -/
 theorem DeletionProp (ω : List B) (hω : ¬ cs.IsReduced ω) : ∃ j < ω.length, ∃ i < j, π ω = π ((ω.eraseIdx j).eraseIdx i) := sorry
+
+/-
+/-! ### The exchange properties and deletion property -/
+
+/-- The right exchange property:
+Let $w = s_{i_1} \cdots s_{i_\ell}$ be a reduced expression for an element $w \in W$
+and let $t \in W$.
+The following are equivalent:
+* $t$ is a right inversion of $w$.
+* $t$ appears in the right inversion sequence of the word $s_{i_1} \cdots s_{i_\ell}$.
+* There exists $j$ with $1 \leq j \leq \ell$ such that
+  $$wt = s_{i_1} \cdots \widehat{s_{i_j}} \cdots s_{i_\ell}$$
+  (i.e. the result of multiplying all of the simple reflections $s_{i_1}, \ldots, s_{i_\ell}$
+  except $s_{i_j}$).
+-/
+theorem right_exchange_tfae_of_reduced {ω : List B} (t : W) (rω : cs.IsReduced ω) :
+    List.TFAE [
+      cs.IsRightInversion (π ω) t,
+      t ∈ ris ω,
+      ∃ j < ω.length, (π ω) * t = π (ω.eraseIdx j)
+    ] := by
+  sorry
+
+/-- The left exchange property:
+Let $w = s_{i_1} \cdots s_{i_\ell}$ be a reduced expression for an element $w \in W$
+and let $t \in W$.
+The following are equivalent:
+* $t$ is a left inversion of $w$.
+* $t$ appears in the left inversion sequence of the word $s_{i_1} \cdots s_{i_\ell}$.
+* There exists $j$ with $1 \leq j \leq \ell$ such that
+  $$tw = s_{i_1} \cdots \widehat{s_{i_j}} \cdots s_{i_\ell}$$
+  (i.e. the result of multiplying all of the simple reflections $s_{i_1}, \ldots, s_{i_\ell}$
+  except $s_{i_j}$).
+-/
+theorem left_exchange_tfae_of_reduced {ω : List B} (t : W) (rω : cs.IsReduced ω) :
+    List.TFAE [
+      cs.IsLeftInversion (π ω) t,
+      t ∈ lis ω,
+      ∃ j < ω.length, t * (π ω) = π (ω.eraseIdx j)
+    ] := by
+  sorry
+
+theorem nodup_rightInvSeq_iff (ω : List B) :
+    List.Nodup (ris ω) ↔ cs.IsReduced ω := by
+  sorry
+
+theorem nodup_leftInvSeq_iff (ω : List B) :
+    List.Nodup (lis ω) ↔ cs.IsReduced ω := by
+  sorry
+
+-/
