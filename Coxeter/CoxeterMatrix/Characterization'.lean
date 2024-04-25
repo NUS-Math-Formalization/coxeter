a4747b002ed6984089bfb40f6dd59b6824f7c941
Chong Jing Quan <jingquan0302@gmail.com>
2024-04-26 01:18:45 +0800

    Finish proof of DeletionProp

--- Coxeter/CoxeterMatrix/Characterization'.lean
+++ Coxeter/CoxeterMatrix/Characterization'.lean
@@ -58,6 +58,7 @@ private lemma not_reduced_imp_not_empty (ω : List B) (h : ¬cs.IsReduced ω) : 
   have := empty_is_reduced cs
   contradiction
 
+-- I prove some properties of maximum index. Could be generalized to arbitrary predicate.
 private lemma max_index_is_not_reduced (ω : List B) (i : ℕ) (h₀ : ¬cs.IsReduced ω)
   (h₁ : i = max_non_reduced_word_index cs ω) : ¬cs.IsReduced (ω.drop i) := by
   rw [← non_reduced_p]
@@ -76,11 +77,7 @@ private lemma beyond_max_index_is_reduced (ω : List B) (n : ℕ) (h₀ : n ≤ 
 
 private lemma max_index_lt_length (ω : List B) (i : ℕ) (h : i = max_non_reduced_word_index cs ω)
   (hω : ¬cs.IsReduced ω) : i < ω.length := by
-  have ω_not_empty : ω ≠ [] := by
-    by_contra!
-    rw [this] at hω
-    have := empty_is_reduced cs
-    contradiction
+  have ω_not_empty := not_reduced_imp_not_empty cs ω hω
   have := @Nat.findGreatest_le (non_reduced_p cs ω) (Classical.decPred _) (List.length ω)
   have i_ne_len : i ≠ ω.length := by
     simp only [max_non_reduced_word_index] at h
@@ -98,35 +95,76 @@ private lemma max_index_lt_length (ω : List B) (i : ℕ) (h : i = max_non_reduc
   rw [h, max_non_reduced_word_index] at *
   exact Nat.lt_of_le_of_ne this i_ne_len
 
-theorem DeletionProp (ω : List B) (hω : ¬cs.IsReduced ω) : ∃ j < ω.length, ∃ i < j, π ω = π ((ω.eraseIdx i).eraseIdx j) := by
-  have ω_not_empty := not_reduced_imp_not_empty cs ω hω
+-- I prove a property of eraseIdx
+private lemma eraseIdx_eq_take_append_drop (ω : List B) (i : ℕ) :
+  ω.eraseIdx i = (ω.take i) ++ (ω.drop (i + 1)) := by
+  induction ω generalizing i with
+  | nil => simp only [List.eraseIdx_nil, List.take_nil, List.drop_nil, List.append_nil]
+  | cons hd tail ih =>
+    by_cases h : i = 0
+    . rw [h]
+      simp only [List.eraseIdx_cons_zero, List.take_zero, zero_add, List.drop_succ_cons,
+        List.drop_zero, List.nil_append]
+    . have := Nat.ne_zero_iff_zero_lt.1 h
+      have := (Nat.sub_eq_iff_eq_add this).mp rfl
+      simp only [List.drop_succ_cons]
+      rw [this]
+      simp only [Nat.reduceSucc, List.eraseIdx_cons_succ, List.take_cons_succ, List.cons_append,
+        List.cons.injEq, true_and]
+      apply ih
+
+-- The proof closely follows Bjorner-Brenti.
+theorem DeletionProp (ω : List B) (hω : ¬cs.IsReduced ω) : ∃ j < ω.length, ∃ i < j, π ω = π ((ω.eraseIdx j).eraseIdx i) := by
   let i := max_non_reduced_word_index cs ω
   have := max_index_lt_length cs ω i rfl hω
   let ω1 := ω.drop i; let ω2 := ω.drop (i + 1)
   have : ω1 ≠ [] := by
-    simp only [ω1]
     apply List.length_pos.1
     rw [List.length_drop]
     omega
   let si := ω1.head this
+  have hd_tail : ω1 = si :: ω2 := by
+    simp only [ω1, ω2, si, (List.tail_drop ω i).symm, List.head_cons_tail]
   have := max_index_is_not_reduced cs ω i hω rfl
   have ω2_reduced : cs.IsReduced ω2 := by apply beyond_max_index_is_reduced; omega; omega
   have : ℓ (π ω1) < ℓ (π ω2) := by
-    have := (List.tail_drop ω i).symm
-    have hd_tail : ω1 = si :: ω2 := by
-      simp only [ω1, ω2, si]; rw [this]; simp only [List.head_cons_tail]
     rw [hd_tail, CoxeterSystem.wordProd_cons]
     by_contra!
     have : ℓ ((s si) * (π ω2)) = ℓ (π ω2) + 1 := by
       apply (CoxeterSystem.length_simple_mul cs (π ω2) si).resolve_right; omega
     have : cs.IsReduced ω1 := by
       show ℓ (π ω1) = ω1.length
-      rw [hd_tail, CoxeterSystem.wordProd_cons, List.length_cons, Nat.succ_eq_add_one, this]
-      simp only [add_left_inj]
+      rw [hd_tail, CoxeterSystem.wordProd_cons, List.length_cons, Nat.succ_eq_add_one, this,
+        add_left_inj]
       exact ω2_reduced
     contradiction
-  have exch_prop : ∃ j < ω.length - i, π ω1 = π (ω2.eraseIdx j) := by sorry
-  sorry
+  have exch_prop : ∃ j < ω2.length, π ω1 = π (ω2.eraseIdx j) := by
+    rw [hd_tail, CoxeterSystem.wordProd_cons]
+    apply left_exchange'
+    rw [CoxeterSystem.IsLeftInversion]
+    constructor
+    . rw [CoxeterSystem.IsReflection]
+      use 1, si
+      simp only [one_mul, inv_one, mul_one]
+    . rw [← CoxeterSystem.wordProd_cons, ← hd_tail]; exact this
+  obtain ⟨j, hj, hj_prod⟩ := exch_prop
+  use (j + i + 1)
+  constructor
+  . simp only [ω2, List.length_drop] at hj; omega
+  . use i
+    constructor
+    . omega
+    . nth_rw 1 [← List.take_append_drop i ω]
+      rw [CoxeterSystem.wordProd_append, eraseIdx_eq_take_append_drop, eraseIdx_eq_take_append_drop,
+        CoxeterSystem.wordProd_append, List.take_append_of_le_length (by rw [List.length_take]; omega),
+        List.take_take, min_eq_left_of_lt (by omega), mul_right_inj,
+        List.drop_append_of_le_length (by rw [List.length_take]; omega), List.drop_take]
+      simp only [Nat.succ_sub_succ_eq_sub, add_tsub_cancel_right]
+      have : j + i + 1 + 1 = (j + 1) + (i + 1) := by exact (Nat.add_right_comm j 1 (i + 1)).symm
+      rw [this]
+      nth_rw 2 [← List.drop_drop]
+      rw [← eraseIdx_eq_take_append_drop]
+      exact hj_prod
 
 /-
 /-! ### The exchange properties and deletion property -/
